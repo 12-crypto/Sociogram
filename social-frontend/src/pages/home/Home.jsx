2ca84dfe507a528e662e5762d83fbce6ae99ae6a
12-crypto <dhruvrathee2000@gmail.com>
2021-08-16 05:25:06 +0530

    added topbar, rightbar, & feed design

--- social-frontend/src/pages/home/Home.jsx
+++ social-frontend/src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
+import Topbar from "../../components/topbar/Topbar.jsx"
+import Sidebar from "../../components/sidebar/Sidebar.jsx"
+import Feed from "../../components/feed/Feed.jsx"
+import Rightbar from "../../components/rightbar/Rightbar.jsx"
 
 
 export default function Home() {
     return (
-        <div>
-         Hello home page   
-        </div>
+        <>
+       
+            <Topbar />
+            <div className="homeContainer" style={{display:'flex'}}>
+                <Sidebar />
+                <Feed/>
+                <Rightbar/>
+            </div>
+            
+        </>
     )
 }
