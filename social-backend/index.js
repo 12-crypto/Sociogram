d524d7c30925cac6c5cf7ecd0cf44d06a07c4460
12-crypto <dhruvrathee2000@gmail.com>
2021-09-18 16:30:56 +0530

    Chatting api
    added REST API for chatting

--- social-backend/index.js
+++ social-backend/index.js
@@ -8,6 +8,8 @@ const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const router = express.Router();
 const path = require("path");
 
@@ -48,6 +50,8 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
